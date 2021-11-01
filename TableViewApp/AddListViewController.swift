2da779a0c74ab3a2c4aad9ccd794662c9f7042c0
yasu-swift <sp4c72q9@icloud.com>
2021-11-01 11:28:38 +0900

    課題完了

--- TableViewApp/AddListViewController.swift
+++ TableViewApp/AddListViewController.swift
@@ -22,11 +22,20 @@ class AddListViewController: UIViewController {
     }
     
     @IBAction func addButton(_ sender: Any) {
-        let userDefaults = UserDefaults.standard //そのままだと長いので変数にいれる
-        taskArray.append(addText.text!) //TextFieldで記入されたテキストを入れる
-        userDefaults.set(taskArray, forKey: "add") //キー"add"で配列をUserDefaultsに保存
+        if addText.text != "" {
+            let userDefaults = UserDefaults.standard //そのままだと長いので変数にいれる
+            taskArray.append(addText.text!) //TextFieldで記入されたテキストを入れる
+            userDefaults.set(taskArray, forKey: "add") //キー"add"で配列をUserDefaultsに保存
+            self.navigationController?.popViewController(animated: true) //1つ前の画面に戻る
+        }else{
+            return
+        }
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
         self.navigationController?.popViewController(animated: true) //1つ前の画面に戻る
     }
+    
     
     /*
      // MARK: - Navigation
